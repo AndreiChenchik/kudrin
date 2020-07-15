5908067ccf5c89f812c785a0f742181dceaa8774
Andrei Chenchik <andrei@chenchik.me>
2020-07-15 18:58:36 +0000

    add link generator

--- main.py
+++ main.py
@@ -12,22 +12,39 @@ import seaborn as sns
 import json
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode, ReplyKeyboardMarkup, KeyboardButton
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# settings
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 NOTION_TOKEN = os.environ['NOTION_TOKEN']
 LIMIT = os.environ['CREDIT_LIMIT']
+POWER_USER_ID = int(os.environ['POWER_USER_ID'])
+POWER_USER_NAME = os.environ['POWER_USER_NAME']
+
+notion_balance = "https://www.notion.so/chenchiks/2062899533a048579f572a7e3d40182f?v=1fb6c93b1a5045af9ea3a83b4aa90dd0" 
+notion_transactions = "https://www.notion.so/chenchiks/1604cc3bb0614273a690710f17b138ca?v=8f278effcac4457d803aeb5cc0a1c93e"
+credit_limit = int(LIMIT)
+
+
+recalculate = "Recalculate"
+newlink = "Update numbers"
+recalculate_keyboard = KeyboardButton(text=recalculate)
+link_keyboard = KeyboardButton(text=newlink)
+custom_keyboard = [[ recalculate_keyboard, link_keyboard ]]
+reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+
+newlink_filter = Filters.text([newlink]) & (Filters.user(user_id=POWER_USER_ID) | Filters.user(username=POWER_USER_NAME))
+recalculate_filter = Filters.text([recalculate]) & (Filters.user(user_id=POWER_USER_ID) | Filters.user(username=POWER_USER_NAME))
+in_known_filters = newlink_filter | recalculate_filter | Filters.command('start')
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!", reply_markup=reply_markup)
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm sorry Dave I'm afraid I can't do that.", reply_markup=reply_markup)
 
 def daily_status(day, date, planned_month, daily):
     return (
@@ -49,7 +66,53 @@ def transactions_made(day, date, planned_month):
         "transaction_amount"
     ].sum()
 
+def generate_link(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="5 sec")
+    now = pd.Timestamp(datetime.now().timestamp(), unit="s").to_period(freq="M")
+    month = now
+    days_in_month = month.days_in_month
+
+    client = NotionClient(token_v2=NOTION_TOKEN)
+    cv = client.get_collection_view(notion_balance)
+    notion_data = [[row.id, row.date.start, row.credit, row.cash, row.usd] for row in cv.collection.get_rows()]
+    balance = pd.DataFrame(notion_data, columns=['id', 'balance_time','Credit', 'Cash', 'USD'])
+
+    balance["balance_year"] = balance["balance_time"].dt.to_period("Y").dt.start_time
+    balance["balance_month"] = balance["balance_time"].dt.to_period("M").dt.start_time
+    balance["balance_day"] = balance["balance_time"].dt.to_period("D").dt.start_time
+    balance["balance_week"] = balance["balance_day"] - balance[
+        "balance_day"
+    ].dt.weekday * np.timedelta64(1, "D")
+
+    yf_exchange = (
+        yf.Ticker("RUB=X")
+        .history(period="max")["Close"]
+        .reset_index()
+        .rename({"index": "date"}, axis=1)
+    )
+    exchange = pd.DataFrame(
+        pd.date_range(start=month.start_time, end=month.end_time)
+    ).rename({0: "date"}, axis=1)
+    exchange["usd_rate"] = exchange["date"].apply(
+        lambda x: yf_exchange[yf_exchange["Date"] <= x].iloc[-1]["Close"]
+    )
+
+    balance = balance.merge(exchange, left_on="balance_day", right_on="date")
+
+    balance["Business"] = (
+        balance["USD"] * balance["usd_rate"]
+    )
+
+    balance["balance"] = (
+        balance["Credit"] - credit_limit + balance["Cash"] + balance["Business"]
+    )
+
+    latest_balance = balance.sort_values(by='balance_time').iloc[-1]
+
+    context.bot.send_message(chat_id=update.effective_chat.id,text=f"*Please fill in* [Balance form](https://docs.google.com/forms/d/e/1FAIpQLSe8JaUuKA22qdeun1MtOUK21LkxXjdcu-yJPUjri-T4Y7m60g/viewform?usp=pp_url&entry.910435313={latest_balance['Credit']}&entry.2073871224={latest_balance['Cash']}&entry.1266770758={latest_balance['USD']}) and after that ask me to recalculate the balance", parse_mode=ParseMode.MARKDOWN)
+
 def recalculate_balance(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Uno momento!")
     # настроим отображение дробных величин
     pd.options.display.float_format = "{:,.2f}".format
 
@@ -58,12 +121,6 @@ def recalculate_balance(update, context):
         context="notebook", style="whitegrid", rc={"figure.figsize": (15, 9)},
     )
     plt.style.use("seaborn-muted")
-
-    # settings
-    notion_balance = "https://www.notion.so/chenchiks/2062899533a048579f572a7e3d40182f?v=1fb6c93b1a5045af9ea3a83b4aa90dd0" 
-    notion_transactions = "https://www.notion.so/chenchiks/1604cc3bb0614273a690710f17b138ca?v=8f278effcac4457d803aeb5cc0a1c93e"
-
-    credit_limit = int(LIMIT)
     
     # get info about this month
     now = pd.Timestamp(datetime.now().timestamp(), unit="s").to_period(freq="M")
@@ -224,11 +281,11 @@ def recalculate_balance(update, context):
     days_after = latest_info['days_after']
     average = latest_info['avg_daily']
 
-    context.bot.send_message(chat_id=update.effective_chat.id,text=f"*Budget status on {diff_date}*")
+    context.bot.send_message(chat_id=update.effective_chat.id,text=f"*Budget status on {diff_date}*", parse_mode=ParseMode.MARKDOWN)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('status.png', 'rb'))
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('spent.png', 'rb'))
-    context.bot.send_message(chat_id=update.effective_chat.id,text=f"Balance: *{diff:+.2f}₽*")
-    context.bot.send_message(chat_id=update.effective_chat.id,text=f"Recommended daily budget: *{new_daily:.0f}₽*")
+    context.bot.send_message(chat_id=update.effective_chat.id,text=f"Balance: *{diff:+.2f}₽*", parse_mode=ParseMode.MARKDOWN)
+    context.bot.send_message(chat_id=update.effective_chat.id,text=f"Recommended daily budget: *{new_daily:.0f}₽*", parse_mode=ParseMode.MARKDOWN)
     context.bot.send_message(chat_id=update.effective_chat.id,text='Have a lovely day ❤️')
 
 def main():
@@ -236,9 +293,9 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-    dp.add_handler(CommandHandler('caps', caps))
-    dp.add_handler(CommandHandler('recalculate_balance', recalculate_balance))
+    dp.add_handler(MessageHandler(~in_known_filters, unknown))
+    dp.add_handler(MessageHandler(recalculate_filter, recalculate_balance))
+    dp.add_handler(MessageHandler(newlink_filter, generate_link))
 
     updater.start_polling()
 
